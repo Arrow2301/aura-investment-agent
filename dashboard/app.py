825795ab03ae219ff7d92b20d49d6595c04d6a0a
Arrow2301 <64690424+Arrow2301@users.noreply.github.com>
2026-09-23 17:54:08 +0530

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import streamlit as st
 from data_engine.universe import SYMBOLS
 st.title('AURA Investment Intelligence')
