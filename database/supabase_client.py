38f167191f73d36cf4fa1b81cd9e257c29059d5b
Arrow2301 <64690424+Arrow2301@users.noreply.github.com>
2026-09-23 15:31:09 +0530

    Update supabase_client.py

--- database/supabase_client.py
+++ database/supabase_client.py
@@ -1,11 +1,15 @@
 import os
-
 from supabase import create_client
 
 
 url = os.getenv("SUPABASE_URL")
-
 key = os.getenv("SUPABASE_ANON_KEY")
+
+
+if not url or not key:
+    raise Exception(
+        "Supabase credentials missing"
+    )
 
 
 supabase = create_client(
